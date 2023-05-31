a79fcbac035ca12acad33717040475dd349b59c5
Bryan6734 <97413577+Bryan6734@users.noreply.github.com>
2023-05-30 22:05:06 -0400

    Better code explanation

--- main.py
+++ main.py
@@ -1,229 +1,91 @@
-# import cv2
-# import numpy as np
-# # import the video 
 
-# # video_path = "IMG_8717.mov"
-# video_path = "output_video.mp4"
-# video = cv2.VideoCapture(video_path)
-
-
-
-
-# # backgroundSubtractor = cv2.createBackgroundSubtractorKNN()
-# backgroundSubtractor = cv2.createBackgroundSubtractorMOG2()
-
-# # kalman = cv2.KalmanFilter(4, 2)
-# # kalman.measurementMatrix = np.array([[1, 0, 0, 0], [0, 0, 1, 0]], np.float32)
-# # kalman.transitionMatrix = np.array([[1, 0, 1, 0], [0, 1, 0, 1], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
-# # kalman.processNoiseCov = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32) * 0.03
-# # kalman.measurementNoiseCov = np.array([[1, 0], [0, 1]], np.float32) * 0.1
-# # kalman.errorCovPost = np.array([[1, 0, 0, 0], [0, 1, 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]], np.float32)
-
-
-# """
-# MOG2 -> models each pixel by a mixture of gaussian distributions
-# -- when a new frame is processed, mog2 compares the pixel instensity with the gaussian distribution
-# -- gaussian distribution: a bell curve that represents the probability of a pixel intensity (0-255) using the mean and the standard deviation
-# -- if the pixel intensity is far from the gaussian distribution, it is considered foreground
-# -- if the pixel intensity is close to the gaussian distribution, it is considered background
-
-# KNN -> models each pixel by selecting K nearest neighbors
-# -- compares pixel intensity with neighbors and decides if it is foreground or background
-# """
-
-# while True:
-#     ret, frame = video.read()
-#     # ret is a boolean (is frame available or no?)
-#     # frame is a numpy pixel array. first 2 dimensions are height and width,
-#     # and the last dimension is the color channel (BGR)
-
-
-
-#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     mask = backgroundSubtractor.apply(gray)
-
-#     # add a blur to the mask
-#     mask = cv2.GaussianBlur(mask, (5, 5), 0)
-
-
-#     # erode the mask
-#     # erode(inputimg, kernel, iterations)
-#     # basically allows us to remove small noise pixels
-#     mask = cv2.erode(mask, None, iterations=3)
-
-#     mask = cv2.dilate(mask, None, iterations=5)
-
-#     # dilate the mask
-#     # dilate(inputimg, kernel, iterations)
-#     # basically allows us to fill in holes
-
-#     # threshold the mask
-#     # threshold(inputimg, thresholdvalue, maxvalue, thresholdtype)
-#     # for the last param, there are different types of thresholding (binary, inversion, 0 for all under threshold, etc)
-#     _, mask = cv2.threshold(mask, 140, 255, cv2.THRESH_BINARY)
-
-#     # find contours
-#     # a contour is a curve joining all continuous points along the boundary of an object (almost like perimeter)
-#     # findContours(inputimg, retrievalmode, approximationmethod)
-#     # retrieval methods:
-#     # --- RETR_EXTERNAL: retrieves only the extreme outer contours (no nesting or child contours)
-#     # --- RETR_LIST: retrieves all contours
-#     # --- RETR_TREE: retrieves all contours and creates a full family hierarchy list
-
-#     # approximation methods:
-#     # --- CHAIN_APPROX_NONE: stores all contour points
-#     # --- CHAIN_APPROX_SIMPLE: compresses horizontal, vertical, and diagonal segments and leaves only their end points
-
-#     contours = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-#     # for contour in contours[0]:
-#     #     # drawContours(inputimg, contours, contourindex, color, thickness)
-#     #     cv2.drawContours(frame, contour, -1, (0, 255, 0), 3)
-#     #     # -1 means draw all contours
-
-#     # # draw all contours to frame
-
-#     # draw lines between all points in the contour
-#     # for contour in contours[0]:
-#     #     for i in range(len(contour) - 1):
-#     #         cv2.line(frame, tuple(contour[i][0]), tuple(contour[i + 1][0]), (0, 255, 0), 2)
-
-#     # draw bounding rectangles around contours
-
-#     window_width = cv2.getWindowImageRect(window_name)[2]
-#     left_shrimp = 0
-#     right_shrimp = 0
-
-#     predicted_position = None
-
-#     # loop over the contours
-#     for contour in contours[0]:
-
-#         # # compute the centroid of the contour
-#         M = cv2.moments(contour)
-#         cx = int(M['m10'] / M['m00'])
-#         cy = int(M['m01'] / M['m00'])
-
-#         # # predict the position of the object using the Kalman filter
-#         # if predicted_position is None:
-#         #     kalman.statePost[0] = cx
-#         #     kalman.statePost[2] = cy
-#         #     predicted_position = (cx, cy)
-#         # else:
-#         #     kalman.predict()
-#         #     predicted_position = (int(kalman.statePost[0]), int(kalman.statePost[2]))
-
-#         # # update the position of the object using the Kalman filter
-#         # z = np.array([[cx], [cy]], dtype=np.float32)
-#         # kalman.correct(z)
-#         # cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
-
-
-#         if cx > window_width / 2:
-#             right_shrimp += 1
-#         else:
-#             left_shrimp += 1
-
-#         cv2.circle(frame, (cx, cy), 5, (0, 0, 255), -1)
-
-#     # for contour in contours[0]:
-#         # # boundingRect(contour) returns (x, y, w, h)
-#         # x, y, w, h = cv2.boundingRect(contour)
-#         # if x < window_width / 2:
-#         #     left_shrimp += 1
-#         # else:
-#         #     right_shrimp += 1
-
-#         # cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 3)
-
-#     cv2.putText(frame, f"Left Shrimp: {left_shrimp}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-#     cv2.putText(frame, f"Right Shrimp: {right_shrimp}", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-
-
-#     # shows frame
-#     cv2.imshow("Brine Shrimp Camera", frame)
-#     cv2.imshow("Mask", mask)
-#     # wait for key press every 1 millisecond
-#     key = cv2.waitKey(1)
-
-#     # if q is pressed, exit
-#     if key == ord('q'):
-#         break
-
-
-# video.release()
-# video.destroyAllWindows()
-
-
+"""
+artemia-opencv
+By Bryan Sukidi
+"""
 
 import cv2
 import matplotlib.pyplot as plt
 
+# Below, I initialized some of the boilerplate needed to open the video and display windows.
 window_name = "Brine Shrimp Camera"
 cv2.namedWindow(window_name)
 
+video_path = "purple-red/purple-red-5.mov"
+video = cv2.VideoCapture(video_path)
 
-# create a background subtractor
-backSub = cv2.createBackgroundSubtractorMOG2()
-
-# create a kernel for morphological operations
-kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-
-# open the video stream
-video = cv2.VideoCapture("purple-red/purple-red-5.mov")
-
+# I keep track of 3 different lists of data which are updated in the main loop.
+# Using these lists, I created a live plot of the data using matplotlib. 
 left_shrimp_record = []
 right_shrimp_record = []
 total_shrimp = []
 
-# create a figure and axis object
 fig, ax = plt.subplots()
 
-# set the x and y limits of the plot
 ax.set_xlim(0, 500)
 ax.set_ylim(0, 40)
 
-# create line objects to plot the data
-line1, = ax.plot([], [], label='Left Shrimp')
-line2, = ax.plot([], [], label='Right Shrimp')
-line3, = ax.plot([], [], label='Max Shrimp')
+left_shrimp_line, = ax.plot([], [], label='Left Shrimp')
+right_shrimp_line, = ax.plot([], [], label='Right Shrimp')
+total_shrimp_line, = ax.plot([], [], label='Max Shrimp')
 
 
-
-# add a legend to the plot
 ax.legend()
-# show the plot
 
+plt.title("Brine Shrimp Distribution")
 plt.xlabel("Frames Processed")
 plt.ylabel("Number of Shrimp")
 plt.show(block=False)
 
+"""
+The MOG2 algorithm is a process that essentially subtracts the background from the foreground.
+It essentially estimates the intensity distribution of the background and then uses that to
+determine the foreground. When you do this a number of times, you can get a pretty good idea of 
+what the foreground is based off what intensities are changing a LOT vs a LITTLE from frame to frame.
+"""
+backgroundSubtractor = cv2.createBackgroundSubtractorMOG2()
 
 while True:
-    # read a frame from the video stream
-    ret, frame = video.read()
 
+    ret, frame = video.read()
     window_width = cv2.getWindowImageRect(window_name)[2]
 
-    # apply background subtraction to obtain the foreground mask
-    fgMask = backSub.apply(frame)
-    fgMask = cv2.GaussianBlur(fgMask, (23, 23), 0)
+    foreground = backgroundSubtractor.apply(frame)
 
-    # apply adaptive thresholding to binarize the image
-    # thresh = cv2.adaptiveThreshold(fgMask, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
-    _, thresh = cv2.threshold(fgMask, 50, 255, cv2.THRESH_BINARY)
+    """
+    The GaussianBlur function takes in a kernel size and uses that to blur the image.
+    Blurring helps us remove noise from the image and make it easier to find contours.
+    The (23, 23) in this case is the value I got after testing a bunch of different values,
+    and it works by taking the average of the 23x23 pixel square around each pixel and calculating
+    pixel intensity based off that average (thereby acheiving a blurring effect).
+    """
+    foreground = cv2.GaussianBlur(foreground, (23, 23), 0)
 
+    # After manually testing values from 0-255, I found that 50 was a good threshold value.
+    # Because we've blurred the image, now each pixel is a different value (intensity) between 0-255
+    # (0 being black and 255 being white). The threshold function essentially converts the image to
+    # black and white based off a threshold value. In ours, IF the intensity is greater than 50, we'll change it to 255,
+    # else if it's less than 50, we'll change it to 0. This sharpens the image a bit.
+    _, thresh = cv2.threshold(foreground, 50, 255, cv2.THRESH_BINARY)
+
+    # The findContours function finds the contours in the image. Contours are essentially the outlines of objects
+    # and it's a function that comes with OpenCV. The RETR_EXTERNAL parameter tells the function to only find the
+    # outermost contours (which is what we want). The CHAIN_APPROX_SIMPLE parameter tells the function to only
+    # return the endpoints of the contours (which is also what we want).
     contours, hierarchy = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
+    # Here, I made two simple variables for counting the shrimp on the left and right side.
     left_shrimp = 0
     right_shrimp = 0
 
     for contour in contours:
+
+        # To get rid of even more noise, I calculated the area of each "blob" in our image in pixels. If the area
+        # is less than 100 pixels, we'll ignore it since it's probably just noise (and almost all of the shrimp are bigger than this).
+        # If it's greater than 100 pixels, we'll draw a circle around it and add it to the left or right shrimp count.
         area = cv2.contourArea(contour)
         if area > 100:
-
             (x, y), radius = cv2.minEnclosingCircle(contour)
-
             if x > window_width / 2:
                 right_shrimp += 1
             else:
@@ -232,44 +94,33 @@ while True:
             cv2.circle(frame, center, 4, (0, 255, 0, 10), -1)
             cv2.putText(frame, str(area), center, cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 0, bottomLeftOrigin=False)
 
+    # The code below is just for updating the lat matplotlib plot. 
+    # I'm keeping track of the number of shrimp on the left, right, and the total.
     left_shrimp_record.append(left_shrimp)
     right_shrimp_record.append(right_shrimp)
     total_shrimp.append(left_shrimp + right_shrimp) 
 
-    line1.set_data(range(len(left_shrimp_record)), left_shrimp_record)
-    line2.set_data(range(len(right_shrimp_record)), right_shrimp_record)
-    line3.set_data(range(len(total_shrimp)), total_shrimp)
+    left_shrimp_line.set_data(range(len(left_shrimp_record)), left_shrimp_record)
+    right_shrimp_line.set_data(range(len(right_shrimp_record)), right_shrimp_record)
+    total_shrimp_line.set_data(range(len(total_shrimp)), total_shrimp)
     fig.canvas.draw()
-
-    # pause for a short time
-    plt.pause(0.01)
-
-    # count the number of brine shrimp on the left vs. the right of the video
-
 
     cv2.putText(frame, f"Left Shrimp: {left_shrimp}", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     cv2.putText(frame, f"Right Shrimp: {right_shrimp}", (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
 
-    # show the output
-    # cv2.imshow("Foreground Mask", fgMask)
-    # cv2.imshow("Thresholded Image", thresh)
+    # Here, I displayed each mask and the original frame. You can see the different steps we went through.
+    # The foreground mask applies the background subtractor (MOG2) and the Gaussian blur.
+    # The threshold mask takes the blurred image and filters it based off the "bright" pixels.
+    # The original frame is just the original frame from the video. We use the threshold mask to calculate
+    # the contours, but we draw everything on the Brine Shrimp camera. 
+    cv2.imshow("Foreground Mask", foreground)
+    cv2.imshow("Thresholded Mask", thresh)
     cv2.imshow("Brine Shrimp Camera", frame)
-    # cv2.imshow("Opening", opening)
 
-    # wait for key press every 1 millisecond
     key = cv2.waitKey(1)
 
-    # if q is pressed, exit
     if key == ord('q'):
         break
 
 
-
-
-
-
-
-
-
 video.release()
-cv2.destroyAllWindows()
